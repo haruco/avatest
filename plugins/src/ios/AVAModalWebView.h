69e80a814e89608477494df73e10630328019b49
haruco <franster_lane17@yahoo.com>
2020-01-13 23:46:47 +0800

    Delete folders

--- plugins/src/ios/AVAModalWebView.h
+++
@@ -1,8 +0,0 @@
-@import Foundation;
-#import <Cordova/CDVPlugin.h>
-
-@interface AVAModalWebView : CDVPlugin
-- (void)init:(CDVInvokedUrlCommand *)command; // TODO method name
-- (void)open:(CDVInvokedUrlCommand *)command;
-@end
-
