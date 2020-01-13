506758bfd0974031401868c4484fa16f20e9af76
haruco <franster_lane17@yahoo.com>
2020-01-13 23:53:09 +0800

    Add files via upload

--- plugins/cordova-plugin-ava-modal/types/index.d.ts
+++ plugins/cordova-plugin-ava-modal/types/index.d.ts
@@ -1,1 +1,14 @@
+declare namespace CordovaPluginModalWebView {
+  interface ModalWebView {
+    open(price: string): void;
+  }
+  interface ModalWebViewStatic {
+    new (closeCallback?: () => void): ModalWebView;
+  }
+}
 
+interface Window {
+  ModalWebView: CordovaPluginModalWebView.ModalWebViewStatic;
+}
+
+declare var ModalWebView: CordovaPluginModalWebView.ModalWebViewStatic;
